6adf3e0524aa1598eead67feae82eeb9989c3593
Christopher Smowton <christopher.smowton@cruk.manchester.ac.uk>
2015-05-28 15:54:55 +0100

    Make bamcmp use the same screwy mixed ordering as 'samtools sort -n'

--- bamcmp.cpp
+++ bamcmp.cpp
@@ -19,6 +19,32 @@ enum cmptypes {
 
 };
 
+// Borrowed from Samtools source, since samtools sort -n uses this ordering:
+
+static int strnum_cmp(const char *_a, const char *_b)
+{
+    const unsigned char *a = (const unsigned char*)_a, *b = (const unsigned char*)_b;
+    const unsigned char *pa = a, *pb = b;
+    while (*pa && *pb) {
+        if (isdigit(*pa) && isdigit(*pb)) {
+            while (*pa == '0') ++pa;
+            while (*pb == '0') ++pb;
+            while (isdigit(*pa) && isdigit(*pb) && *pa == *pb) ++pa, ++pb;
+            if (isdigit(*pa) && isdigit(*pb)) {
+                int i = 0;
+                while (isdigit(pa[i]) && isdigit(pb[i])) ++i;
+                return isdigit(pa[i])? 1 : isdigit(pb[i])? -1 : (int)*pa - (int)*pb;
+            } else if (isdigit(*pa)) return 1;
+            else if (isdigit(*pb)) return -1;
+            else if (pa - a != pb - b) return pa - a < pb - b? 1 : -1;
+        } else {
+            if (*pa != *pb) return (int)*pa - (int)*pb;
+            ++pa; ++pb;
+        }
+    }
+    return *pa? 1 : *pb? -1 : 0;
+}
+
 static void usage() {
 
   fprintf(stderr, "Usage: intersect -a input1.s/b/cram -b input2.s/b/cram [-m matching_records.s/b/cram] [-1 first_only.s/b/cram] [-2 second_only.s/b/cram] [-t nthreads] [-B iobuffersize] [-c comparison_type]\n");
@@ -347,7 +373,7 @@ int main(int argc, char** argv) {
 	  sam_write1(second_out, header1, seqs2.recs[idx2]);
 
     }
-    else if(qname1 < qname2) {
+    else if(strnum_cmp(qname1.c_str(), qname2.c_str()) < 0) {
 
       if(first_out)
 	sam_write1(first_out, header1, in1.rec);
